99f915eeaf471da7eae43ad2bccf7b60925d1a40
Gabriel Casas <gcasas132@gmail.com>
2025-08-08 10:02:20 -0300

    cors

--- alma-backend/mysite/settings.py
+++ alma-backend/mysite/settings.py
@@ -73,7 +73,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",  # O el puerto que uses en Vue
+    "http://127.0.0.1:8080",
+]
 
 ROOT_URLCONF = 'mysite.urls'
 
