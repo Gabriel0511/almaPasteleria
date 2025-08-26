26e37820c7d4a00c81600a6d1b624def5a23d096
Gabriel <gcasas132@gmail.com>
2025-08-26 20:07:36 -0300

    admin

--- alma-backend/insumos/admin.py
+++ alma-backend/insumos/admin.py
@@ -1,9 +1,27 @@
 from django.contrib import admin
 from .models import Insumo, Proveedor, CategoriaInsumo, UnidadMedida
-# Register your models here.
 
-admin.site.register(Insumo)
-admin.site.register(Proveedor)
-admin.site.register(CategoriaInsumo)
-admin.site.register(UnidadMedida)
+# --- ADMIN CLASSES SIMPLIFICADAS ---
+class InsumoAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'categoria', 'unidad_medida', 'stock_actual', 'stock_minimo', 'proveedor']
+    list_filter = ['categoria', 'unidad_medida', 'proveedor']
+    search_fields = ['nombre']
+    list_editable = ['stock_actual', 'stock_minimo']
 
+class ProveedorAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'telefono', 'email']
+    search_fields = ['nombre', 'email']
+
+class CategoriaInsumoAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+    search_fields = ['nombre']
+
+class UnidadMedidaAdmin(admin.ModelAdmin):
+    list_display = ['nombre', 'abreviatura']
+    search_fields = ['nombre', 'abreviatura']
+
+# --- REGISTROS SIMPLES ---
+admin.site.register(Insumo, InsumoAdmin)
+admin.site.register(Proveedor, ProveedorAdmin)
+admin.site.register(CategoriaInsumo, CategoriaInsumoAdmin)
+admin.site.register(UnidadMedida, UnidadMedidaAdmin)
