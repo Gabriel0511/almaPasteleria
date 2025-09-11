1246549473936646b9b65527f54d3f0072fa023d
Gabriel <gcasas132@gmail.com>
2025-09-11 19:18:13 -0300

    secretkey temporal

--- alma-backend/mysite/settings.py
+++ alma-backend/mysite/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # Cambia la SECRET_KEY para usar variables de entorno
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY', default='clave-temporal-para-deploy-inicial-solo')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 # Configuración más segura para production
