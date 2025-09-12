209f3102d67c32674dd35f7136faf77c76cd4cc6
Gabriel <gcasas132@gmail.com>
2025-09-11 23:51:44 -0300

    cors

--- alma-backend/mysite/settings.py
+++ alma-backend/mysite/settings.py
@@ -111,7 +111,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOWED_ORIGINS = [
     "https://almapasteleria-production-a6ba.up.railway.app",
-    "https://fascinating-cannoli-25b9bb.netlify.app",
+    "https://sensational-gaufre-639a67.netlify.app",
     "http://localhost:8080",
     "http://127.0.0.1:8080", 
     "http://localhost:5173",
