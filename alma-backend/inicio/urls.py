6316a0b695a21429ca81e7e29205f1d1eb99beb5
Gabriel <gcasas132@gmail.com>
2025-12-03 13:06:26 -0300

    Eliminado recuperacion de contraseñá

--- alma-backend/inicio/urls.py
+++ alma-backend/inicio/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from .views import (
     RegistroView, LoginView, PerfilView,
-    VerifyTokenView, change_password, LogoutView,
-    PasswordResetRequestView, VerifyResetCodeView, ResetPasswordView
+    VerifyTokenView, change_password, LogoutView
 )
 
 urlpatterns = [
@@ -12,8 +11,4 @@ urlpatterns = [
     path('verify/', VerifyTokenView.as_view(), name='token-verify'),
     path('change-password/', change_password, name='change_password'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    # Recuperación de contraseña
-    path('password-reset/', PasswordResetRequestView.as_view(), name='password-reset'),
-    path('verify-reset-code/', VerifyResetCodeView.as_view(), name='verify-reset-code'),
-    path('reset-password/', ResetPasswordView.as_view(), name='reset-password'),
 ]
