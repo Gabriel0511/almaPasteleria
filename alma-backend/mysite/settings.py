cd374459623b751cb54201e4f0867a3a8b8d1c4a
Gabriel <gcasas132@gmail.com>
2026-02-12 16:21:23 -0300

    cors

--- alma-backend/mysite/settings.py
+++ alma-backend/mysite/settings.py
@@ -108,7 +108,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://sensational-gaufre-639a67.netlify.app",
     "http://localhost:8080",
     "http://127.0.0.1:8080", 
-    "http://localhost:5174",
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
